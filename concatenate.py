5d6d3d6cf258d08dbe7bd30b2c0e7b2f18c1925f
Eleu Carlos <31489452+eleucarlos@users.noreply.github.com>
2021-01-27 15:28:24 -0300

    update append and log

--- concatenate.py
+++ concatenate.py
@@ -9,24 +9,34 @@ start = time.process_time()
 class InputError(Exception):
     pass
 
+workspace_path = r"D:\expresso2o365\bi"
+workspace_new  = PurePath(workspace_path, "new")
+workspace_old  = PurePath(workspace_path, "old")
+workspace_done = PurePath(workspace_path, "done")
+workspace_log = PurePath(workspace_path, "log")
+file_log_name  = "base_email1.csv"
+
+if not Path(workspace_log).exists():
+        logging.warning("Diretório de arquivos dos logs não existe, o mesmo sera criado")
+        Path(workspace_log).mkdir()
+        logging.warning(f"Criado o diretório de log: {workspace_log}")
+
+if not Path(workspace_new).exists():
+        logging.warning("Diretório de arquivos novos não existe, o mesmo sera criado")
+        Path(workspace_new).mkdir()
+        logging.warning(f"Criado o diretório de arquivos novos: {workspace_new}")
+
 handler = logging.StreamHandler()
 formater = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s')
 handler.setFormatter(formater)
 
 logging.getLogger().addHandler(handler)
 
-handler = logging.FileHandler(filename="concatenate.log", mode="a", encoding="utf-8")
+handler = logging.FileHandler(filename=f"{workspace_log}/concatenate.log", mode="a", encoding="utf-8")
 handler.setFormatter(formater)  
 
 logging.getLogger().addHandler(handler)
 logging.getLogger().setLevel(logging.DEBUG)
-
-
-workspace_path = "/home/lgro/git/parseLog"
-workspace_new  = PurePath(workspace_path, "new")
-workspace_old  = PurePath(workspace_path, "old")
-workspace_done = PurePath(workspace_path, "done")
-file_log_name  = "log-concatenado.csv"
 
 files = Path(workspace_new)
 
@@ -44,27 +54,32 @@ else:
     logging.debug(f"Lista dos arquivos encontrados: {', '.join(files)}")
 
     if not Path(workspace_done).exists():
-        logging.warning("Diretório de log não exite, o mesmo sera criado")
+        logging.warning("Diretório concluído não existe, o mesmo sera criado")
         Path(workspace_done).mkdir()
-        logging.warning(f"Criado o diretório de log: {workspace_done}")
+        logging.warning(f"Criado o diretório done: {workspace_done}")
 
     if not Path(workspace_old).exists():
-        logging.warning("Diretório de arquivos processados não exite, o mesmo sera criado")
+        logging.warning("Diretório old para arquivos processados não existe, o mesmo sera criado")
         Path(workspace_old).mkdir()
-        logging.warning(f"Criado o diretório de log: {workspace_old}")
+        logging.warning(f"Criado o diretório old para processados: {workspace_old}")
+
+    if not Path(workspace_log).exists():
+        logging.warning("Diretório de arquivos dos logs não existe, o mesmo sera criado")
+        Path(workspace_log).mkdir()
+        logging.warning(f"Criado o diretório de log: {workspace_log}")
 
     try:
         file_log = PurePath(workspace_done, file_log_name)
         logging.info(f"Gerando o arquivo concatenado: {file_log}")
-        with open(file_log, "w") as outfile:
+        with open(file_log, "a") as outfile:
             for indice, arquivo in enumerate(files, start=1):
-                logging.info(f"Contatenando o arquivo [{indice}/{len(files)}]: {PurePath(workspace_new, arquivo)}")
+                logging.info(f"Concatenando o arquivo [{indice}/{len(files)}]: {PurePath(workspace_new, arquivo)}")
                 with open(PurePath(workspace_new, arquivo), "r") as infile:
                     outfile.write(infile.read())
                     logging.info(f"Conteudo do arquivo gravado com sucesso: {PurePath(workspace_new, arquivo)}")
                 logging.info(f"Movendo arquivo para o diretorio old: {PurePath(workspace_old, arquivo)}")
                 Path(PurePath(workspace_new, arquivo)).rename(PurePath(workspace_old, arquivo))
-            outfile.write("\n")
+            #outfile.write("\n")
         stop = time.process_time()
         logging.info(f"Todos os arquivos foram processados com sucesso em: {stop - start}s")
     except Exception as e:
